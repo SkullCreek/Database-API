e856ba30b1d6ac2a99bc39113ad159dc590beaaa
Darpanarayan Bahadur <92776555+SkullCreek@users.noreply.github.com>
2022-12-30 22:48:03 +0530

    postgreSQL CRUD

--- connection.py
+++ connection.py
@@ -1,27 +1,73 @@
+import psycopg2 as pst
 from sqlalchemy import create_engine
 import pandas as pd
 import urllib
 import logging
-logging.basicConfig(filename="connection.log",level=logging.DEBUG,format=("%(levelname)s %(level)s %(message)s"))
+
 
 class PostgreSql:
-    def __init__(self,user,host,port,password):
+    logging.basicConfig(filename="connection.log", level=logging.DEBUG)
+    def __init__(self,user,host,port,password,dbname):
         self.user = user
         self.host = host
         self.port = port
         self.password = password
+        self.dbname = dbname
     def connect(self):
         try:
-            self.uri = "postgresql+psycopg2://"+self.user+":" + urllib.parse.quote(self.password) + "@"+self.host+":"+self.port+"/pandas"
+            self.uri = "postgresql+psycopg2://"+self.user+":" + urllib.parse.quote(self.password) + "@"+self.host+":"+self.port+"/"+self.dbname+""
             self.engine = create_engine(self.uri)
         except Exception as e:
-            logging.CRITICAL(e)
+            logging.critical(e)
     def insert(self,link,name):
         try:
             self.df = pd.read_csv(link)
             self.df.to_sql(name=name,con=self.engine,if_exists='replace',index=False)
         except Exception as e:
-            logging.CRITICAL(e)
+            logging.error(e)
+    def update(self,table_name,set,condition):
+        try:
+            self.con = pst.connect(database=self.dbname,user=self.user,host=self.host,port=self.port,password=self.password)
+            self.cur = self.con.cursor()
+            self.query = "UPDATE "+table_name+" SET "+set+" WHERE "+ condition
+            logging.info(self.query)
+            self.cur.execute(self.query)
+            self.con.commit()
+        except Exception as e:
+            logging.warning(e)
+        finally:
+            if(self.con):
+                self.cur.close()
+                self.con.close()
+
+    def delete(self,table_name,condition):
+        try:
+            self.con = pst.connect(database=self.dbname,user=self.user,host=self.host,port=self.port,password=self.password)
+            self.cur = self.con.cursor()
+            self.query = "DELETE FROM "+table_name+" WHERE "+condition
+            self.cur.execute(self.query)
+            self.con.commit()
+        except Exception as e:
+            logging.warning(e)
+        finally:
+            if(self.con):
+                self.cur.close()
+                self.con.close()
+
+    def select(self, row, table_name, condition):
+        try:
+            self.con = pst.connect(database=self.dbname,user=self.user,host=self.host,port=self.port,password=self.password)
+            self.cur = self.con.cursor()
+            self.query = "SELECT "+row+" FROM "+table_name+" WHERE "+condition
+            self.cur.execute(self.query)
+            self.con.commit()
+            return self.cur.fetchall()
+        except Exception as e:
+            logging.warning(e)
+        finally:
+            if(self.con):
+                self.cur.close()
+                self.con.close()
 
 
 
--- main.py
+++ main.py
@@ -1,1 +1,67 @@
+import connection as con
+from flask import Flask,request,jsonify
 
+app = Flask(__name__)
+
+@app.route('/postgresql/insert',methods=['GET','POST'])
+def create_table():
+    if(request.method == 'POST'):
+        user = request.json['user']
+        host = request.json['host']
+        port = request.json['port']
+        password = request.json['password']
+        dbname = request.json['dbname']
+        dataset = request.json['dataset']
+        dataset_name = request.json['dataset_name']
+        postgre_conn = con.PostgreSql(user,host,port,password,dbname)
+        postgre_conn.connect()
+        postgre_conn.insert(dataset,dataset_name)
+        return jsonify("successfully inserted data")
+
+@app.route('/postgresql/update',methods=['GET','POST'])
+def update_table():
+    if (request.method == 'POST'):
+        user = request.json['user']
+        host = request.json['host']
+        port = request.json['port']
+        password = request.json['password']
+        dbname = request.json['dbname']
+        table_name = request.json['table_name']
+        setp = request.json['set']
+        condition = request.json['condition']
+        postgre_conn = con.PostgreSql(user, host, port, password, dbname)
+        postgre_conn.update(table_name,setp,condition)
+        return jsonify("successfully updated data")
+
+@app.route('/postgresql/delete',methods=['GET','POST'])
+def delete_table():
+    if (request.method == 'POST'):
+        user = request.json['user']
+        host = request.json['host']
+        port = request.json['port']
+        password = request.json['password']
+        dbname = request.json['dbname']
+        table_name = request.json['table_name']
+        condition = request.json['condition']
+        postgre_conn = con.PostgreSql(user, host, port, password, dbname)
+        postgre_conn.delete(table_name,condition)
+        return jsonify("successfully updated data")
+
+@app.route('/postgresql/select',methods=['GET','POST'])
+def select_table():
+    if (request.method == 'POST'):
+        user = request.json['user']
+        host = request.json['host']
+        port = request.json['port']
+        password = request.json['password']
+        dbname = request.json['dbname']
+        row = request.json['row']
+        table_name = request.json['table_name']
+        condition = request.json['condition']
+        postgre_conn = con.PostgreSql(user, host, port, password, dbname)
+        ans = postgre_conn.select(row,table_name,condition)
+        return jsonify(ans)
+
+
+if __name__ == "__main__":
+    app.run()
